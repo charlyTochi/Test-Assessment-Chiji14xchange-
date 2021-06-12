db029b270af743df217d6d986e77816be30741f6
charlyTochi <orafu@gmail.com>
2021-06-13 00:52:08 +0100

    Updates

---
+++ precache-manifest.043ca7f36a5c485c9c31c89c27566a83.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b552ab94a33efaa87a85fd673ebaa0c",
+    "url": "/Test-Assessment-Chiji14xchange-/index.html"
+  },
+  {
+    "revision": "0c1bbebbdf0af1921baa",
+    "url": "/Test-Assessment-Chiji14xchange-/static/css/2.800b4633.chunk.css"
+  },
+  {
+    "revision": "62d221d9f05fa83a4372",
+    "url": "/Test-Assessment-Chiji14xchange-/static/css/main.643c10f5.chunk.css"
+  },
+  {
+    "revision": "0c1bbebbdf0af1921baa",
+    "url": "/Test-Assessment-Chiji14xchange-/static/js/2.7a187e49.chunk.js"
+  },
+  {
+    "revision": "e0ca77513391a36b97aaec145e7053f6",
+    "url": "/Test-Assessment-Chiji14xchange-/static/js/2.7a187e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d221d9f05fa83a4372",
+    "url": "/Test-Assessment-Chiji14xchange-/static/js/main.5e631c39.chunk.js"
+  },
+  {
+    "revision": "48aec1387fc43702cd95",
+    "url": "/Test-Assessment-Chiji14xchange-/static/js/runtime-main.64f58cc4.js"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/Test-Assessment-Chiji14xchange-/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/Test-Assessment-Chiji14xchange-/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/Test-Assessment-Chiji14xchange-/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/Test-Assessment-Chiji14xchange-/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
